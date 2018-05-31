a983a6703682810f1a4623cf55fad5aa1ffe1c2d
aliaimviz <im.irfanmumtaz21@gmail.com>
2018-05-31 10:55:33 +0500

    fixing media objects

--- aboutus.php
+++ aboutus.php
@@ -4,13 +4,13 @@
       <div class="row">
          <div class="col-md-12">
             <h3 class="about_text">About Us</h3>
-            <p class="f_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim<br>veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate<br> velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id<br> est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut<br>enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit<br>in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt<br>mollit anim id est laborum.
+            <p class="f_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id  est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
-            <br>
-            <p class="f_content">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo<br>inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut<br> fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor<br> sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat<br> voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi<br> consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum<br> fugiat quo voluptas nulla pariature.
+             
+            <p class="f_content">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut  fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor  sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat  voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi  consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum  fugiat quo voluptas nulla pariature.
             </p>
-            <br>
-            <p class="f_content">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas<br> molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum<br> fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit<br> quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et<br> aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum<br> hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+             
+            <p class="f_content">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas  molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum  fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit  quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et  aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum  hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
             </p>
          </div>
       </div>
@@ -20,35 +20,35 @@
    <div class="container">
       <div class="row">
          <div class="col-md-4 col-sm-6 col-xs-12">
-            <div class="media">
-               <div class="media-left">
-                  <img src="assets/images/activity_image1.jpg" class="media-object">
+            <div class="row">
+               <div class="col-md-3 col-sm-12 col-xs-12">
+                  <img src="assets/images/activity_image1.jpg" class="">
                </div>
-               <div class="media-body">
-                  <h4 class="media-heading f_special">Special Activities</h4>
-                  <p class="f_lorem">Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit,<br> sed do eiusmod tempor.</p>
+               <div class="col-md-9 col-sm-12 col-xs-12">
+                  <h4 class="f_special">Special Activities</h4>
+                  <p class="f_lorem">Lorem ipsum dolor sit amet,  consectetur adipiscing elit,  sed do eiusmod tempor.</p>
                </div>
             </div>
          </div>
          <div class="col-md-4 col-sm-6 col-xs-12">
-            <div class="media">
-               <div class="media-left">
-                  <img src="assets/images/activity_image2.jpg" class="media-object">
+            <div class="row ">
+               <div class="col-md-3 col-sm-12 col-xs-12">
+                  <img src="assets/images/activity_image2.jpg" class="">
                </div>
-               <div class="media-body">
-                  <h4 class="media-heading f_special">Travel Arrangements</h4>
-                  <p class="f_lorem">Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit,<br> sed do eiusmod tempor.</p>
+               <div class="col-md-9 col-sm-12 col-xs-12">
+                  <h4 class="f_special">Travel Arrangements</h4>
+                  <p class="f_lorem">Lorem ipsum dolor sit amet,  consectetur adipiscing elit,  sed do eiusmod tempor.</p>
                </div>
             </div>
          </div>
          <div class="col-md-4 col-sm-6 col-xs-12 col-md-offset-0 col-sm-offset-3 col-xs-offset-0">
-            <div class="media f_responsive_last">
-               <div class="media-left">
-                  <img src="assets/images/activity_image3.jpg" class="media-object">
+            <div class="row f_responsive_last">
+               <div class="col-md-3 col-sm-12 col-xs-12">
+                  <img src="assets/images/activity_image3.jpg" class="">
                </div>
-               <div class="media-body">
-                  <h4 class="media-heading f_special">Location Manager</h4>
-                  <p class="f_lorem">Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit,<br> sed do eiusmod tempor.</p>
+               <div class="col-md-9 col-sm-12 col-xs-12">
+                  <h4 class="f_special">Location Manager</h4>
+                  <p class="f_lorem">Lorem ipsum dolor sit amet,  consectetur adipiscing elit,  sed do eiusmod tempor.</p>
                </div>
             </div>
          </div>
@@ -61,40 +61,40 @@
          <div class="col-md-12">
             <h3 class="about_text">Team</h3>
          </div>
-         <div class="media f_about_media">
-            <div class="media-left f_responsive">
-               <img src="assets/images/team_image1.jpg" class="media-object">
+         <div class="row f_about_media">
+            <div class="col-md-3 col-sm-12 col-xs-12 f_responsive">
+               <img src="assets/images/team_image1.jpg" class="">
             </div>
-            <div class="media-body f_padding_media">
-               <h4 class="media-heading f_name">John Doe<br><span>Founder and Chief Executive Officer</span></h4>
-               <p class="f_ipsum">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum<br> deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,<br> similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum<br> quidem rerum facilis est et expedita distinction.</p>
-            </div>
-         </div>
-         <div class="media f_about_media">
-            <div class="media-left f_responsive">
-               <img src="assets/images/team_image2.jpg" class="media-object">
-            </div>
-            <div class="media-body f_padding_media">
-               <h4 class="media-heading f_name">Joseph Mark<br><span>Co-founder & Chief Commercial Officer</span></h4>
-               <p class="f_ipsum">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum<br> deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,<br> similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum<br> quidem rerum facilis est et expedita distinction.</p>
+            <div class="col-md-9 col-sm-12 col-xs-12 f_padding_media">
+               <h4 class="f_name">John Doe <span>Founder and Chief Executive Officer</span></h4>
+               <p class="f_ipsum">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum  deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,  similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum  quidem rerum facilis est et expedita distinction.</p>
             </div>
          </div>
-         <div class="media f_about_media">
-            <div class="media-left f_responsive">
-               <img src="assets/images/team_image3.jpg" class="media-object">
+         <div class="row f_about_media">
+            <div class="col-md-3 col-sm-12 col-xs-12 f_responsive">
+               <img src="assets/images/team_image2.jpg" class="">
             </div>
-            <div class="media-body f_padding_media">
-               <h4 class="media-heading f_name">Maria Dose<br><span>Chief Marketing Officer</span></h4>
-               <p class="f_ipsum">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum<br> deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,<br> similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum<br> quidem rerum facilis est et expedita distinction.</p>
+            <div class="col-md-9 col-sm-12 col-xs-12 f_padding_media">
+               <h4 class="f_name">Joseph Mark <span>Co-founder & Chief Commercial Officer</span></h4>
+               <p class="f_ipsum">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum  deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,  similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum  quidem rerum facilis est et expedita distinction.</p>
             </div>
          </div>
-         <div class="media f_about_media">
-            <div class="media-left f_responsive">
-               <img src="assets/images/team_image4.jpg" class="media-object">
+         <div class="row f_about_media">
+            <div class="col-md-3 col-sm-12 col-xs-12 f_responsive">
+               <img src="assets/images/team_image3.jpg" class="">
             </div>
-            <div class="media-body f_padding_media">
-               <h4 class="media-heading f_name">John Doe<br><span>Chief Operations Officer</span></h4>
-               <p class="f_ipsum">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum<br> deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,<br> similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum<br> quidem rerum facilis est et expedita distinction.</p>
+            <div class="col-md-9 col-sm-12 col-xs-12 f_padding_media">
+               <h4 class="f_name">Maria Dose <span>Chief Marketing Officer</span></h4>
+               <p class="f_ipsum">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum  deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,  similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum  quidem rerum facilis est et expedita distinction.</p>
+            </div>
+         </div>
+         <div class="row f_about_media">
+            <div class="col-md-3 col-sm-12 col-xs-12 f_responsive">
+               <img src="assets/images/team_image4.jpg" class="">
+            </div>
+            <div class="col-md-9 col-sm-12 col-xs-12 f_padding_media">
+               <h4 class="f_name">John Doe <span>Chief Operations Officer</span></h4>
+               <p class="f_ipsum">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum  deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,  similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum  quidem rerum facilis est et expedita distinction.</p>
             </div>
          </div>
       </div>
