5561c9a3ddcd3697e08e92a622c9d0981a552b79
Sheheryar Nadeem <Sheheryar.n@aimviz.com>
2018-05-31 12:36:15 +0500

    shehryar_work

---
+++ hotels.php
@@ -0,0 +1,27 @@
+<?php include('header.php'); ?>
+<section class="hotel">
+	<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h3 class="about_text">Select Hotels</h3>
+
+			<ul class="list_right">
+                              <li><a href="#">Search</a></li>
+                             <i class="fa fa-angle-right f_iconright"></i>
+                              <li><a href="#">Select Hotels</a></li>
+                              <i class="fa fa-angle-right f_iconright"></i>
+                              <li><a href="#">Select Rooms</a></li>
+                             <i class="fa fa-angle-right f_iconright"></i>
+                              <li><a href="#">Payment</a></li>
+                             <i class="fa fa-angle-right f_iconright"></i>
+                              <li><a href="#">Confirmation</a></li>
+                              
+                              
+                           </ul>
+		</div>
+
+
+	</div>
+</div>
+</section>
+<?php include('footer.php'); ?>
